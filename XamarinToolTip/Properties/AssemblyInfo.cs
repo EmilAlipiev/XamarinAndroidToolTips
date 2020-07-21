db49dae40f69d54b1a81ccb3d5b351d326317e77
Emil Alipiev <emil_alipiev@hotmail.com>
2020-07-21 22:52:33 +0200

    renamed

--- XamarinToolTip/Properties/AssemblyInfo.cs
+++ XamarinToolTip/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("XamarinToolTip")]
+[assembly: AssemblyTitle("XamarinAndroidToolTips")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("XamarinToolTip")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("XamarinAndroidToolTips")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
